c953da3374eca8e2d0e38375a7fccf7e97d12f19
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-08-09 13:48:39 -0400

    update setup.py to match requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
 	name = 'FacebookGraph',
 	version = '1.0.0',
-	install_requires = ['requests==2.0.0', 'simplejson==3.3.0',],
+	install_requires = ['requests>=2.0.0', 'simplejson>=3.3.0',],
 	description = 'Python wrapper for Facebook Graph API',
 	author = 'Marcis Kalnins',
 	author_email = 'marcis@xid.lv',
